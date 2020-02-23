ee7da11053637e1e183fb94e71226704ffb1d120
Tetsuro <tetsuro.takara@gmail.com>
2020-02-23 15:59:17 -0500

    Start adding button

---
+++ src/components/GeneratorButton/GeneratorButton.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function GeneratorButton() {
+  return <button>Click to generate chord</button>;
+}
---
+++ src/components/GeneratorButton/index.js
@@ -0,0 +1,1 @@
+export { GeneratorButton } from './GeneratorButton';
